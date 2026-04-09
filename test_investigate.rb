725b57f01b48a158255ebc548e3e13379ec62388
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2026-04-10 05:56:00 +0900

    remove temporary files

--- test_investigate.rb
+++
@@ -1,8 +0,0 @@
-require 'open3'
-env = ENV.to_hash
-server = spawn(env, "bundle exec ruby server.rb", out: 'server_test.log', err: 'server_test.log')
-sleep 3
-client = spawn(env, "bundle exec ruby client.rb 20000", out: 'client_test.log', err: 'client_test.log')
-sleep 15
-Process.kill("KILL", client) rescue nil
-Process.kill("KILL", server) rescue nil
